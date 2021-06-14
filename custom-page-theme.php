868b99c37f88265a205a3a3c71ad52bb0bc87e7a
Brittney Cavazos <brittney.cavazos@gmail.com>
2021-06-14 14:39:55 -0700

    feat(page)-added custom a custom page template

---
+++ custom-page-theme.php
@@ -0,0 +1,42 @@
+<?php
+
+/* Template Name: CustomPageTheme */ 
+
+
+get_header();
+
+?>
+
+<main id="primary" class="site-main">
+
+
+<div class="container">
+        <div class="row">
+            <div class="col-4">
+            <p>playing with the text</p>
+            <button class="btn btn-primary">
+                button text
+            </button>
+            </div>
+        </div>
+    </div>
+
+<?php
+while ( have_posts() ) :
+    the_post();
+
+    get_template_part( 'template-parts/content', 'page' );
+
+    // If comments are open or we have at least one comment, load up the comment template.
+    if ( comments_open() || get_comments_number() ) :
+        comments_template();
+    endif;
+
+endwhile; // End of the loop.
+?>
+
+</main><!-- #main -->
+
+<?php
+get_sidebar();
+get_footer();
